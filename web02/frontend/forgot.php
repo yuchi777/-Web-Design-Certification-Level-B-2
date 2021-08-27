be5ea2319c1904d11795cfa9c27776d4fe5504e5
yuchi777 <yuchi8943@gmail.com>
2021-08-27 16:01:07 +0800

    建置註冊及忘記密碼頁面

---
+++ web02/frontend/forgot.php
@@ -0,0 +1,23 @@
+<fieldset>
+    <legend>
+        忘記密碼
+    </legend>
+    <table>
+        <tr>
+            <td>請輸入信箱以查詢密碼</td>
+        </tr>
+        <tr>
+            <td>
+                <input type="text" name="email" id="">
+            </td>
+        </tr>
+        <tr>
+            <td id="result"></td>
+        </tr>
+        <tr>
+            <td>
+                <button>尋找</button>
+            </td>
+        </tr>
+    </table>
+</fieldset>
--- web02/frontend/reg.php
+++ web02/frontend/reg.php
@@ -1,1 +1,33 @@
-123
+<fieldset>
+    <legend>
+        會員註冊
+    </legend>
+    <div style="color:red">*請設定您要註冊的帳號及密碼(最長12個字元)</div>
+    <!-- table>tr*5>td+td:input:text  -->
+    <table>
+        <tr>
+            <td class="clo">Step1:登入帳號</td>
+            <td><input type="text" name="" id=""></td>
+        </tr>
+        <tr>
+            <td class="clo">Step2:登入密碼</td>
+            <td><input type="text" name="" id=""></td>
+        </tr>
+        <tr>
+            <td class="clo">Step3:再次確認密碼</td>
+            <td><input type="text" name="" id=""></td>
+        </tr>
+        <tr>
+            <td class="clo">Step4:信箱(忘記密碼時使用)</td>
+            <td><input type="text" name="" id=""></td>
+        </tr>
+        <tr>
+            <td>
+                <input type="button" value="註冊">
+                <input type="reset" value="清除">
+            </td>
+            <td>
+            </td>
+        </tr>
+    </table>
+</fieldset>
