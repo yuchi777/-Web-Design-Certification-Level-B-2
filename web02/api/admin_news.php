98365215cbf6c3845d6a6fcba0c5bb710507b93a
yuchi777 <yuchi8943@gmail.com>
2021-08-29 11:24:27 +0800

    完成最新文章的顯示設定及刪除功能

--- web02/api/admin_news.php
+++ web02/api/admin_news.php
@@ -1,14 +1,26 @@
 <?php
 include '../base.php';
 
-// print_r($_POST['del']);
+print_r($_POST['id']);
 
-if($_POST['del']){
-    foreach ($_POST['del'] as $key => $id) {
-        $mem=$Mem->del($id);
+
+foreach ($_POST['id'] as $value => $id) {
+
+    // 判斷del 
+    if(isset($_POST['del']) && in_array($id,$_POST['del'])){
+        $News->del($id);
+    }else{
+        // 判斷sh
+        $news=$News->find($id);
+
+        //三元運算子
+        $news['sh'] = (isset($_POST['sh']) && in_array($id,$_POST['sh']))?1:0;
+        $News->save($news);
+
     }
 }
 
-to("../backend.php?do=acc");
+
+to("../backend.php?do=news");
 
 ?>
